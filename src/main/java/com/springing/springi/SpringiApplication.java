8cacc4ab4c583781d326442332b03504d4598af6
VaviRerma <rraviverma98@gmail.com>
2023-08-17 02:15:55 +0530

    initialize springi

---
+++ src/main/java/com/springing/springi/SpringiApplication.java
@@ -0,0 +1,13 @@
+package com.springing.springi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springing/springi/SpringiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springing.springi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
